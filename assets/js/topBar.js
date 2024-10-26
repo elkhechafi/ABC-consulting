75885cbd662dfbae937cfb7b89b7abd2cbe71c98
elkhechafi <elkhechafizakaria@gmail.com>
2024-10-26 14:44:22 +0100

    update the responsivness of the website on 600px

--- assets/js/topBar.js
+++ assets/js/topBar.js
@@ -9,3 +9,15 @@ function topBarBackground() {
 };
 
 window.onscroll = function() {topBarBackground()};
+
+
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } 
